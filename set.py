d6b88ab3aa482441ba5bdebc983196802c240585
PrateekMittal1008 <redaliensco@gmail.com>
2022-02-25 20:14:30 +0530

    array

--- set.py
+++ set.py
@@ -1,1 +1,12 @@
-#set
+#sets:-update, add, clear,remove, diff-update,copy, differnce, intersection
+set1 ={43,56,55,33}
+set2 = {33,55,66,33,88} 
+set1.add(4)
+print(set1)
+set1.add(5)
+set2.clear()
+print(set2)
+set1.remove(43)
+print(set1)
+set1.update(45)
+print(set1)
