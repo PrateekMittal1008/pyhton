dc8e3897b7a35526fdea4903f85587d38732d005
PrateekMittal1008 <redaliensco@gmail.com>
2022-02-25 15:41:25 +0530

    init

--- tuple.py
+++ tuple.py
@@ -1,3 +1,10 @@
-#Tuple, Dict, 
-z = (34,54,34,22,6,73,76)
-print(z)
+#Tuple >>index, count
+tup1 = (34,54,34,22,6,73,76)
+tup2 = (4,6,7,8,33,77)
+print(tup1)
+#Tuple count
+count=tup1.count(34)
+print(count)
+#tuple index value search
+ser = tup1.index(54)
+print(ser)
