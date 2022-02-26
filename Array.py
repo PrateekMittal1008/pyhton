fb0b7dd09bb3d0529c462bedb1093d5dae18bb61
PrateekMittal1008 <redaliensco@gmail.com>
2022-02-26 11:28:06 +0530

    init

--- Array.py
+++ Array.py
@@ -25,3 +25,7 @@ print(val1)
 val1.remove(3) # Remove elements in array
 print(val1)
 print(val1[2]) # print directly elements from array
+
+# loops with array
+for i in val1:
+    print(i, end=" ")
