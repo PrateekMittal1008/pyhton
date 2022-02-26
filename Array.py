6d502831389c5f2788dbe130af0a3c0375ac416e
PrateekMittal1008 <redaliensco@gmail.com>
2022-02-26 11:03:35 +0530

    init

--- Array.py
+++ Array.py
@@ -1,5 +1,16 @@
-#Array
+#Array and thier functions
+#append, buffer info, clear, copy, count, ectend, index,insert, pop, remove, reverse, sort
 from array import * # import all functions from array lib
-array1 = array('i',[4,6,5,3,5,7])
-print(array1.
-  
+val1 = array('i',[4,6,5,3,5,7])
+val2 = array('i',[4,5,7,8,4,7])
+print(val1)
+print(val2)
+print(type(val1))
+#append:add single element to last
+val1.append(7)
+print(val1)
+print(val1.buffer_info())# Returns the address and size of array 
+cont = val1.count(7) # count number of type value returned
+print(cont)
+val1.copy(val2) # copy the e
+print(val2)
